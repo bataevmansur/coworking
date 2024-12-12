ec6d69aab5b2386fac9af47cdaab7a71458d2a91
YusufNoob <yusufyurtay@gmail.com>
2024-12-12 10:25:10 +0100

    aanpassing script.js

--- tests/test_API/script.js
+++ tests/test_API/script.js
@@ -1,24 +1,25 @@
-let weather = {
+let weather = {  // nieuw object weer
     apiKey: "4941e02079f27bac2345e598003b222c",
     fetchWeather: function (city) {
         fetch(
             `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}&units=metric`
+            // fetch data van een gekozen API
         )
         .then((response) => response.json())
         .then((data) => this.displayWeather(data))
         .catch((error) => console.error("Error fetching the weather data:", error));
     },
-    displayWeather: function (data) {
-        const { name } = data;
-        const { temp } = data.main;
-        const { description } = data.weather[0];
+    displayWeather: function (data) {   // functie om data op scherm te krijgen
+        const { name } = data;          // constante name = naam gegeven stad
+        const { temp } = data.main;     // constante temp = temperatuur
+        const { description } = data.weather[0];    // constante description = weeromstandigheden
         const weatherInfo = `
-            <h2>Weather in ${name}</h2>
-            <p>Temperature: ${temp}°C</p>
+            <h2>Weather in ${name}</h2>         
+            <p>Temperature: ${temp}°C</p>   // alle info in 1 variabele
             <p>Condition: ${description}</p>
         `;
-        document.getElementById("weather-info").innerHTML = weatherInfo;
+        document.getElementById("weather-info").innerHTML = weatherInfo;    //info op scherm tonen
     },
 };
 
-weather.fetchWeather("Gent");
+weather.fetchWeather("Gent");   //roept functie op
